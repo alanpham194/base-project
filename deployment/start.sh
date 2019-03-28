bd7f7f266b87d6995a2ea18e049a97a0b1896585
alan <hoak57uet@gmail.com>
2019-03-28 10:24:12 +0700

    add properties file

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,2 +1,4 @@
+mkdir /home/ubuntu/env/
 cd /home/ubuntu/travala/travala-users
+aws s3 cp s3://trav-env/trav-dev2/travala_users_dev.properties /home/ubuntu/env/users.properties
 sudo docker-compose up -d
