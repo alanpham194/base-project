efea6ef415fd8342b40fbdbfce27ca7ae06f24fe
alan <hoak57uet@gmail.com>
2019-03-28 15:10:49 +0700

    off docker container before start

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,4 +1,5 @@
 mkdir /home/ubuntu/env/
 cd /home/ubuntu/travala/travala-searching
 aws s3 cp s3://trav-env/trav-dev2/travala_searching_dev.properties /home/ubuntu/env/searching.properties
+sudo docker-compose down
 sudo docker-compose up -d
